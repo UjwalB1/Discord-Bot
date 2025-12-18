4e3621aa9a57d95e6fde38bf752d003e4b855bbb
Ujwal Bastola <ubastola90@gmail.com>
2025-12-18 15:20:56 -0500

    Attempting to set up small database with json so the bot doesn't forget data if it stops running

--- cogs/general.py
+++ cogs/general.py
@@ -6,27 +6,64 @@ import json
 class General(commands.Cog):
     def __init__(self, bot : commands.Bot):
         self.bot = bot
+        self.welcome_channel_data = "welcome_channel.json"
+
+        if not os.path.exists(self.welcome_channel_data):
+            with open(self.welcome_channel_data, "w") as f:
+                json.dump({}, f)
+
+
+    def load_settings(self): # load json file fn to read
+        with open(self.welcome_channel_data, "r") as f:
+            return json.load(f)
+
+    def save_settings(self, data): # savee json file fn to write new data/overwrite
+        with open(self.welcome_channel_data, "w") as f:
+            json.dump(data, f, indent=4)
+
+
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"We are ready to go in, {self.bot.user.name}")
+        print(f"Bot is ready to be deployed, war veteran bot dealing with randoms lol, {self.bot.user.name}")
 
 
     @commands.command()
     @commands.has_permissions(manage_guild=True)
     async def set_welcome_channel(self, ctx, channel: discord.TextChannel):
-        server_id = ctx.guild.id
-        await ctx.send(f"Welcome channel has been set to{channel.mention}")
+        data = self.load_settings()
+        data[str(ctx.guild.id)] = channel.id
+        self.save_settings(data)
+
+        await ctx.send(f"Welcome channel has been set to {channel.mention}")
 
 
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        server_id = member.guild.id
-        channel = discord.utils.get(member.guild.channels, name="general")
+        data = self.load_settings()
+        server_id = str(member.guild.id)
+        target_channel = None
 
-        if channel:
-            await channel.send(f"Welcome to the server {member.mention}!")
+        if server_id in data:
+            target_channel = self.bot.get_channel(int(data[server_id]))
+
+        if not target_channel: # try system chan if the welcome channel hasn't been setup
+            target_channel = member.guild.system_channel
+
+        # final resort just use the first channel possible in the server
+        if not target_channel:
+            for channel in member.guild.text_channels:
+                if channel.permissions_for(member.guild.me).send_messages:
+                    target_channel = channel
+                    break
+
+
+        if target_channel: # send message if valid channel found
+            try:
+                await target_channel.send(f"Welcome to the server {member.mention}!")
+            except discord.Forbidden:
+                print(f"Permission error in {member.guild.name}")
 
 
 
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
-import asyncio
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
-handler = logging.FileHandler(filename="discord.log", encoding ="utf-8", mode = 'w')
+
+base_dir = os.path.dirname(os.path.abspath(__file__)) # each time I run, the discord.log file no longer be outside expected dir
+log_path = os.path.join(base_dir, "discord.log")
+handler = logging.FileHandler(filename=log_path, encoding ="utf-8", mode = 'w')
 
 class UjiBot(commands.Bot):
     def __init__(self):
@@ -17,8 +19,9 @@ class UjiBot(commands.Bot):
         super().__init__(command_prefix="uji ", intents=intents)
 
     # load cogs before bot connects
-    async def setup_c(self):
-        for filename in os.listdir("./cogs"): 
+    async def setup_hook(self):
+        cogs_path = os.path.join(base_dir, "cogs")
+        for filename in os.listdir(cogs_path): 
             if filename.endswith(".py"):
                 await self.load_extension(f"cogs.{filename[:-3]}")
 
