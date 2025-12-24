a8e45e385d49013d9f425e9cc2bfc0389b733715
Ujwal Bastola <ubastola90@gmail.com>
2025-12-24 15:36:15 -0500

    Used Gemini API to make  a summarizer function which summarizes the chat using the most recent 200 messages

--- cogs/summarizer.py
+++ cogs/summarizer.py
@@ -1,11 +1,44 @@
+import datetime
 import discord
+import os
 from discord.ext import commands
 from discord import app_commands
+from google import genai
+from google.genai import types
 
 class Summarizer(commands.Cog):
     def __init__(self, bot : commands.Bot):
         self.bot = bot
+        self.client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
 
+
+
+    @commands.hybrid_command(name="summarize", description="Uji bot summarizes the last 200 messages in the channel, ignoring bot messages.")
+    async def summarize(self, ctx):
+        await ctx.defer()
+        messages = []
+
+        async for msg in ctx.channel.history(limit=200):
+            if not msg.author.bot and msg.content:
+                messages.append(f"{msg.author.display_name}: {msg.clean_content}") # ignore irrelevant messages n also format for gemini to understand stuff better
+
+        messages.reverse()
+
+        message_history = "\n".join(messages)
+
+        prompt = f"You go by the name 'Uji bot,' and will summarize the message_history, in a non-serious joking manner, while still being concise and occassionally poke fun at certain things in the conversation and give opinions on it for jokes:\n\n {message_history}"
+
+        response = self.client.models.generate_content( # gen the text output and hold it within the response var
+            model="gemini-2.5-flash",
+            contents=prompt,
+            config=types.GenerateContentConfig(
+                thinking_config=types.ThinkingConfig(thinking_budget=0) # disable thinking
+            ),
+        )
+
+        embed_summarizer = discord.Embed(title="What you missed", description=response.text, colour=discord.Colour.blue(), timestamp=datetime.datetime.now(datetime.timezone.utc))
+
+        await ctx.reply(embed = embed_summarizer)
 
 
 
